aef3a968e5edbf57ee411adf1cbc46f80ae47d0a
rafael vincent <raf.deguzman@gmail.com>
2024-08-29 01:19:41 -0400

    progress has been made

--- app/page.tsx
+++ app/page.tsx
@@ -57,6 +57,76 @@ export default function Home() {
         </p>
       </div>
 
+      <div className="flex flex-col items-start justify-center h-[400px] w-full max-w-4xl mx-auto">
+        <h2 className="text-gruvbox-fg text-4xl font-bold mb-4">education</h2>
+        <ul className="text-gruvbox-fg max-w-2xl">
+          <li>
+            <span className="font-bold">B.CompSc - Concordia University:</span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              Computer Science
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">
+              DEC / Associate's Degree - John Abbott College:
+            </span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              Computer Science
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col items-start justify-center h-[400px] w-full max-w-4xl mx-auto">
+        <h2 className="text-gruvbox-fg text-4xl font-bold mb-4">
+          work experience
+        </h2>
+        <ul className="text-gruvbox-fg max-w-2xl">
+          <li>
+            <span className="font-bold">Acculete:</span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              Mobile App Developer
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Chargehub:</span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              Software Developer Intern
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col items-start justify-center h-[400px] w-full max-w-4xl mx-auto">
+        <h2 className="text-gruvbox-fg text-4xl font-bold mb-4">projects</h2>
+        <ul className="text-gruvbox-fg max-w-2xl">
+          <li>
+            <span className="font-bold">Cat Trader:</span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              Tinder-like cat swiping app
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Fantasy Survivors:</span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              Top-down bullet hell game
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Bananabank:</span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              .NET Maui application for managing personal finances
+            </span>
+          </li>
+          <li>
+            <span className="font-bold">Cropbox:</span>
+            <span className="text-gruvbox-fg text-sm font-normal">
+              Full stack android application for managing an automated green
+              house
+            </span>
+          </li>
+        </ul>
+      </div>
       {/*
           models: use polypizza google models (?)
           should i add a blog section???????????????
