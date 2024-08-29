30f6a07c410e05d2b4f2b4d93ca48d9ab0b84d35
rafael vincent <raf.deguzman@gmail.com>
2024-08-29 03:03:32 -0400

    fixing config bcoz its not working :<

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/rafdeguzman.github.io",
+  assetPrefix: "/rafdeguzman.github.io/",
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
