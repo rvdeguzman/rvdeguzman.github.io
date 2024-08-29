ce3295fb46692138f36cd42d63ea77df65ca014d
rafael vincent <raf.deguzman@gmail.com>
2024-08-29 03:04:34 -0400

    new build

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/rafdeguzman.github.io",
-  assetPrefix: "/rafdeguzman.github.io/",
-  output: "export",
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
