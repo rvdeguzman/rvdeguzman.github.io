1139591974023ae5e5b356c80c685dc2a9c36c03
rafael vincent <raf.deguzman@gmail.com>
2024-08-28 23:02:26 -0400

    changed configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: "/rafdeguzman.github.io",
   assetPrefix: "/rafdeguzman.github.io/",
   output: "export",
