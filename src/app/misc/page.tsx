e6c2394e82b924511323d1da8eec2343a0532a6b
rvdeguzman <rvdeguzman@protonmail.com>
2025-10-15 21:30:37 -0400

    wip

--- src/app/misc/page.tsx
+++ src/app/misc/page.tsx
@@ -8,24 +8,24 @@ export default function Misc() {
                 <h1 className="text-3xl font-bold mb-4">Misc</h1>
                 <div className="space-y-8">
                     <section>
-                        <h2 className="text-lg font-medium mb-4">Lorem Ipsum</h2>
-                        <div className="space-y-4 text-sm text-gray-600 dark:text-gray-400">
+                        <h2 className="text-2xl font-medium">Lorem Ipsum</h2>
+                        <div className="space-y-2 text-sm text-gray-400 mt-1 mb-2">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                         </div>
                     </section>
                     <section>
-                        <h2 className="text-lg font-medium mb-4">Lorem Ipsum</h2>
-                        <div className="space-y-4 text-sm text-gray-600 dark:text-gray-400">
+                        <h2 className="text-2xl font-medium">Lorem Ipsum</h2>
+                        <div className="space-y-2 text-sm text-gray-400">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                         </div>
                     </section>
                     <section>
-                        <h2 className="text-lg font-medium mb-4">Lorem Ipsum</h2>
-                        <div className="space-y-4 text-sm text-gray-600 dark:text-gray-400">
+                        <h2 className="text-2xl font-medium">Lorem Ipsum</h2>
+                        <div className="space-y-2 text-sm text-gray-400">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
