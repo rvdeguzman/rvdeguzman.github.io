259bedf1ff83838e43a2eef404e858f42d67a907
rv <rvdeguzman@protonmail.com>
2026-02-10 14:43:26 -0500

    feat: removed dots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
                 suppressHydrationWarning={true}
             >
                 <ThemeProvider enableSystem={false} attribute="class">
-                    <main className="min-h-screen bg-white dark:bg-black bg-[radial-gradient(#222222_1px,transparent_1px)] dark:bg-[radial-gradient(#222222_1px,transparent_1px)] [background-size:32px_32px]">
+                    <main className="min-h-screen bg-white dark:bg-black">
                         {children}
                     </main>
                 </ThemeProvider>
